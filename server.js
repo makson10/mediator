025bd34a5aa7e3f19cbcf9086dc6cea1ef1f94b2
Maks <makson94021@gmail.com>
2023-02-25 13:46:50 +0200

    server app v1.0.3

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const PORT = 4000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 
 app.get('/', (req, res) => {
-    res.render('mainPage');
+    res.render('index');
 });
 
 
@@ -37,7 +37,7 @@ app.route('/lessons')
 
 
 app.listen(PORT, () => {
-    console.log(`Server was started on http://127.0.0.1:${PORT}`);
+    console.log(`Server was started on ${PORT}`);
 });
 
 //* code to future
@@ -48,3 +48,82 @@ app.listen(PORT, () => {
 //         'Content-Type': 'application/json'
 //     },
 // });
+
+
+// * fuck html code 
+// <!DOCTYPE html>
+// <html lang="en">
+// 	<head>
+// 		<meta charset="UTF-8" />
+// 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+// 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+// 		<title>Mediator</title>
+// 		<style>
+// 			body {
+// 				overflow: hidden;
+// 				min-height: 100vh;
+// 				font-family: sans-serif;
+
+// 				display: flex;
+// 				flex-direction: column;
+// 				gap: 40px;
+// 				justify-content: center;
+// 				align-items: center;
+// 			}
+
+// 			#mediator-logo {
+// 				width: 150px;
+// 				height: 150px;
+// 			}
+
+// 			#mediator-text-wrapper {
+// 				display: flex;
+// 				flex-direction: column;
+// 				gap: 10px;
+// 			}
+
+// 			#mediator-title {
+// 				width: 100%;
+// 				font-size: 3rem;
+// 				text-align: center;
+// 				font-weight: bold;
+
+// 				animation: titleAnim 4000ms linear infinite;
+// 			}
+
+// 			#mediator-subtitle {
+// 				font-size: 1.2rem;
+// 			}
+
+// 			@keyframes titleAnim {
+// 				0%,
+// 				100% {
+// 					text-shadow: 0 0 5px blue;
+// 				}
+
+// 				25% {
+// 					text-shadow: 0 0 5px yellow;
+// 				}
+
+// 				50% {
+// 					text-shadow: 0 0 5px red;
+// 				}
+
+// 				75% {
+// 					text-shadow: 0 0 5px green;
+// 				}
+// 			}
+// 		</style>
+// 	</head>
+// 	<body>
+// 		<img
+// 			id="mediator-logo"
+// 			src="https://img.icons8.com/officel/200/000000/mediator.png" />
+// 		<div id="mediator-text-wrapper">
+// 			<span id="mediator-title">Mediator</span>
+// 			<span id="mediator-subtitle">
+// 				Intermediate server between PHPBoyFriend and LessonReminder
+// 			</span>
+// 		</div>
+// 	</body>
+// </html>
